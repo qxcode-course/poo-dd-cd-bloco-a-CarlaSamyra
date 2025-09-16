e5fe681815c304376001b27f3b29286d409f8981
Samyra Torres <samyratorres476@gmail.com>
2025-09-16 20:25:50 +0000

    Fazendo a atividade da pasta git_pratica, pasta toalha

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,19 @@
+class Towel: 
+    def __init__(self, color: str, size: str): #construtor
+        self.color = color #atributos
+        self.size = size
+        self.wetness = 0
+    
+    def __str__(self):
+        return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}"
+
+towel = Towel("green", "G") #objetos
+toalhas = Towel("red", "M") #objetos
+
+print(towel.color)
+towel.color = "White"
+print(towel.color)
+print(towel.size)
+print(towel.wetness)
+
+print(towel)
