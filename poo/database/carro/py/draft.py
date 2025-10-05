1766949cf26d9ebb3aa6e788842d4dfa4e69bd35
Samyra Torres <samyratorres476@gmail.com>
2025-10-05 14:37:10 -0300

    Fazendo pequenos ajustes no código

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,69 +1,70 @@
 class Car:
-    def __init__ (self, pas: int, gas: int, km: int):
-        self.pas = 0
-        self.gas = 0
-        self.km = 0
-        self.gasMax = 100 
-        self.pasMax = 100
+    def __init__ (self, pas: int, km: int, gas: int):
+        self.pas: int = 0 
+        self.km: int = 0
+        self.gas: int = 0
 
     def __str__ (self):
         return f"pass: {self.pas}, gas: {self.gas}, km: {self.km}"
     
-    def enter (self):
+    def pasMax (self):
         self.pas += 1
         if self.pas > 2:
             print ("fail: limite de pessoas atingido")
             self.pas = 2
-
+    
     def leave (self):
         self.pas -= 1
         if self.pas < 0:
             print ("fail: nao ha ninguem no carro")
             self.pas = 0
 
-    def fuel (self, increment: int):
-        self.gas += increment
-        if self.gas > self.gasMax:
+    def gasMax (self, amount: int):
+        self.gas += amount 
+        if self.gas > 100:
             self.gas = 100
-        
+
+    
     def drive (self, distance: int):
+        if self.gas == 0:
+            print ("fail: tanque vazio")
+            return
         if self.pas == 0:
             print ("fail: nao ha ninguem no carro")
-        elif self.gas == 0:
-            print ("fail: tanque vazio")
-        elif distance > self.gas:
-            distancia = self.gas
+            return 
+        if distance > self.gas:
+            distancia = self.gas 
             print (f"fail: tanque vazio apos andar {distancia} km")
+            self.km = self.km + distancia 
             self.gas = 0
-            self.km += distancia
-        else:
-            self.km += distance
+            return
+        if distance < self.gas:
             self.gas -= distance
+            self.km = distance 
+            return
 
-            
 
 def main():
-    carro = Car ("", "", "")
+    carro = Car ("", "", "") 
+
     while True:
         line = input()
         args: list[str] = line.split(" ")
         print ("$" + line)
+
         if args[0] == "end":
             break
         elif args[0] == "show":
             print(carro)
         elif args[0] == "enter":
-            carro.enter()
+            carro.pasMax()
         elif args[0] == "leave":
             carro.leave()
         elif args[0] == "fuel":
-            increment: int = int(args[1])
-            carro.fuel(increment)
+            amount: int = int (args[1])
+            carro.gasMax(amount)
         elif args[0] == "drive":
-            distance: int = int(args[1])
+            distance: int = int (args[1])
             carro.drive(distance)
-        else: 
-            print ("fail: comando inválido")
-
 
 main()
