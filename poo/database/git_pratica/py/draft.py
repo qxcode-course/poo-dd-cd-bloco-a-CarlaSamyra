54a7c35fec59f616d2d5b27d9b1ccc80ac33b664
Samyra Torres <samyratorres476@gmail.com>
2025-10-05 23:10:53 -0300

    adicionando relatório da prática git

---
+++ poo/database/git_pratica/py/draft.py
@@ -0,0 +1,9 @@
+    # Sobre as atividades so git prática. A maioria dos testes que deveriam ser feitos fiz na sala de aula mesmo, seguindo os passos do professor. 
+    # Alguns dos testes fiz nesse repositório, até tinha criado uma pasta aqui mas acabei apagando ela e deixando só as que tinham atividades mesmo. 
+    # Consegui fazer a maioria dos testes, desde o criar uma pasta até o criar conflitos, até me ajudou a entender melhor como a lógica funciona mais ou 
+    # menos e consegui resolver alguns dos conflitos que eu mesma criei durante as atividades do tko.
+    # Dentre todos os testes, o que mais tive dificuldade foi o de utilizar o vscode. Fiz todo o processo de criar a chave ssh e fiz diversos testes com outros repositórios 
+    # meus para entender como funcionava os push e etc. Consegui conectar meu vscode com o github, configurar o tko e baixar tudo que era necessário para realizar as atividades.
+    # Definitivamente a maior dificuldade foi conectar tudo, mas depois de feito, consegui realizar 80% das atividades pela minha máquina local, era até mais fácil por conta da internet.
+    # Por alguns problemas, que não faço ideia do porque acontecem, precisei finalizar algumas atividades pelo codespace mesmo, tko dava alguns bugs que eu não conseguia realizar os testes.
+    # No mais, tirando toda a parte difícil de entender como configurar o vscode e esses bugs do tko, consegui desenvolver bem as práticas com os comandos git. 
